0dae8ff12c90367a67e928bdc4593e98804a13bc
Lydia Puric <lydia.puric@gmail.com>
2021-06-23 23:44:29 +0200

    init

---
+++ src/main/java/com/lyd/springgqpoc/SpringGqPocApplication.java
@@ -0,0 +1,13 @@
+package com.lyd.springgqpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGqPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGqPocApplication.class, args);
+	}
+
+}
